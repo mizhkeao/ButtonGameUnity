76b4ef1bcd674501a037e04cb8a6c920851b3731
mizhkeao <55338389+mizhkeao@users.noreply.github.com>
2023-10-08 23:49:42 -0700

    first commit

---
+++ button_quantum/quantum.code/Platformer/PlayerSpawnSystem.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quantum.Platformer
+{
+    unsafe class PlayerSpawnSystem : SystemSignalsOnly, ISignalOnPlayerDataSet
+    {
+        public void OnPlayerDataSet(Frame f, PlayerRef player)
+        {
+            var data = f.GetPlayerData(player);
+
+            // resolve the reference to the prototpye.
+            var prototype = f.FindAsset<EntityPrototype>(data.CharacterPrototype.Id);
+
+            // Create a new entity for the player based on the prototype.
+            var entity = f.Create(prototype);
+
+            // Create a PlayerLink component. Initialize it with the player. Add the component to the player entity.
+            var playerLink = new PlayerLink()
+            {
+                Player = player,
+            };
+            f.Add(entity, playerLink);
+
+            // Offset the instantiated object in the world, based in its ID.
+            if (f.Unsafe.TryGetPointer<Transform3D>(entity, out var transform))
+            {
+                transform->Position.X = 0 + player;
+            }
+        }
+    }
+}
